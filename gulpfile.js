ea9a704a96b9f6c509161adc20097418103c0735
Strokov Ivan <Strokovhii@gmail.com>
2020-01-26 14:31:44 +0700

    Правка ошибок и неточностей

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,18 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -32,11 +44,19 @@ gulp.task("css", function () {
     .pipe(gulp.dest("build/css"));
 });
 
+//gulp.task("js", function () {
+//  return pipeline(
+//        gulp.src('source/js/*.js'),
+//        uglify(),
+//        gulp.dest("build/js")
+//  );
+//});
+
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
-//    imagemin.jpegtran({progressive: true}),
+      imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"));
@@ -55,14 +75,6 @@ gulp.task("sprite", function () {
     }))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
 });
 
 gulp.task("copy", function () {
